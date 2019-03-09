ad2e75529b1a735361c9624957c4a0ef66240c60
franlol <fraaaaan@gmail.com>
2019-03-09 21:06:13 +0100

    more upload fixed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const Recipe = require('../models/Recipe');
 
 const { requireUser } = require('../middlewares/auth');
 
-router.get('/', requireUser, (req, res, next) => {
-    res.render('index', { title: 'MyFoody' });
+router.get('/', requireUser, async (req, res, next) => {
+    try {
+        const recipes = await Recipe.find({});
+        console.log(recipes);
+        res.render('index', { recipes });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;

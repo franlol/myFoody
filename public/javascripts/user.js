adf5bf0398b0066bfe0e54745b242c8d4189c5ee
franlol <fraaaaan@gmail.com>
2019-03-12 00:24:43 +0100

    more frontend

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -10,14 +10,6 @@ const main = () => {
             window.location.replace('/recipes/' + id);
         });
     });
-
-    // Al pulsar el boton del user, el cual pertenece a un form, redirigir a profile
-    const buttonProfile = document.getElementById('main-button-user');
-    buttonProfile.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log('asd');
-        window.location.href += 'user';
-    });
 };
 
 window.addEventListener('load', main);
